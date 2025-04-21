26b806ddb25e2c94416fbdeb66ffd3f467f296fe
autores-uk <autores.uk@protonmail.me>
2025-04-21 14:16:12 +0100

    Remove accidentally committed code

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,50 +1,12 @@
 package com.example;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.infra.Blackhole;
-import uk.autores.Messages;
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import uk.autores.Texts;
 
-@Messages("i18n.properties")
+@Texts("world.txt")
 public class Hello {
-    public static void main( String[] args ) throws IOException {
-//        URLClassLoader classLoader = (URLClassLoader) Hello.class.getClassLoader();
-//        StringBuilder classpath = new StringBuilder();
-//        for (URL url : classLoader.getURLs()) {
-//            classpath.append(url.getPath()).append(File.pathSeparator);
-//        }
-//        System.setProperty("java.class.path", classpath.toString());
-
-        org.openjdk.jmh.Main.main(args);
-    }
-
-    private static final Locale[] LOCALES = {
-            Locale.ROOT,
-            Locale.ENGLISH,
-            Locale.FRENCH,
-            Locale.ITALIAN,
-            Locale.GERMAN,
-    };
-
-    @Benchmark
-    public void method(Blackhole blackhole) {
-        for (Locale l : LOCALES) {
-            String s = I18N.iso80000Defines(l, "GB", 1024*1024*1024);
-            blackhole.consume(s);
-        }
-    }
-
-    @Benchmark
-    public void bundle(Blackhole blackhole) {
-        for (Locale l : LOCALES) {
-            ResourceBundle bundle = ResourceBundle.getBundle("com.example.i18n", l);
-            String template = bundle.getString("iso-80000-defines");
-            MessageFormat m = new MessageFormat(template, l);
-            String s = m.format(new Object[]{"GB", 1024*1024*1024});
-            blackhole.consume(s);
-        }
+    public static void main( String[] args ) {
+        // Uses package name for generated class if not set explicitly
+        String value = Example.world();
+        System.out.println("Hello " + value);
     }
 }
