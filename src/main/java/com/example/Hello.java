62651bdf871dc1f46e65595d500f0e233dd8856b
autores-uk <autores.uk@protonmail.me>
2025-04-21 14:07:10 +0100

    Updated tutorial to 8.0.41-beta

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -3,13 +3,10 @@ package com.example;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.infra.Blackhole;
 import uk.autores.Messages;
-import uk.autores.Texts;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 @Messages("i18n.properties")
 public class Hello {
@@ -35,12 +32,19 @@ public class Hello {
     @Benchmark
     public void method(Blackhole blackhole) {
         for (Locale l : LOCALES) {
-
+            String s = I18N.iso80000Defines(l, "GB", 1024*1024*1024);
+            blackhole.consume(s);
         }
     }
 
     @Benchmark
-    public void bundle() {
-        // Do nothing
+    public void bundle(Blackhole blackhole) {
+        for (Locale l : LOCALES) {
+            ResourceBundle bundle = ResourceBundle.getBundle("com.example.i18n", l);
+            String template = bundle.getString("iso-80000-defines");
+            MessageFormat m = new MessageFormat(template, l);
+            String s = m.format(new Object[]{"GB", 1024*1024*1024});
+            blackhole.consume(s);
+        }
     }
 }
